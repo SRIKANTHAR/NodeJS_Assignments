6c04fc5f8c453e8031ea1248b82f3dc353fd5101
SRIKANTHAR <srikanthmugadalr52@gmail.com>
2022-03-07 14:52:58 +0530

    My Node JS Assignment_1 submission

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,17 +1,29 @@
+const { name } = require("tar/lib/types");
+
 function getNameFromCommandLine() {
-    // Write you code here, name should be taken as args in process.argv
+  // Write you code here, name should be taken as args in process.argv
+  var name = process.argv[process.argv.length-1];
+  return name;
 }
+
 
 function getNameFromEnv() {
-    // Write your code here
+  // Write your code here
+  return process.env.name;
 }
 
-function getNameFromReadLine() {
-    // Write your code here
+
+function getNameFromReadLine(name) {
+  // Write your code here
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 }
+
 
 module.exports = {
-    getNameFromCommandLine,
-    getNameFromEnv,
-    getNameFromReadLine
-}
+  getNameFromCommandLine,
+  getNameFromEnv,
+  getNameFromReadLine,
+};
